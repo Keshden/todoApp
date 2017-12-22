aa1cc82c884addce99ffd3091e164355e1dfc60e
Keshden <bleonrey7@gmail.com>
2017-12-21 20:59:23 -0700

    create the edit and update actions and views

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -15,11 +15,17 @@ class TodosController < ApplicationController
   end
 
   def edit
-
+    @todo = Todo.find(params[:id])
   end
 
   def update
-
+    @todo = Todo.find(params[:id])
+    if @todo.update(todo_params)
+      flash[:success] = "Todo was successfully updated"
+      redirect_to todo_path(@todo)
+    else
+      render 'edit'
+    end
   end
 
   def show
